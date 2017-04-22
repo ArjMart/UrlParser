e95d61f38d1c7e6acdc31e346c12729ba3d2a00c
Arjun Vikram <arjvik@gmail.com>
2017-04-21 19:49:23 -0500

    Added AllTests JUnit Test Suite

---
+++ test/com/arjvik/arjmart/urlparser/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.arjvik.arjmart.urlparser.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UrlParametersMapTest.class, UrlParserTest.class })
+public class AllTests {
+
+}
